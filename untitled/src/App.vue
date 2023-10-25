251315012a95842587d2d0f3d6c7de05fee3abf2
dokhung <ehhiop123@naver.com>
2023-10-25 13:49:58 +0900

    vue.js first

---
+++ untitled/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+<script>
+
+</script>
+
+<template>
+<!-- 여기가 메인 바디 -->
+<div></div>
+</template>
+
+<style scoped>
+// 여기가 css
+
+
+
+
+
+
+</style>
